5246c726ade81d381164794aa9c94e7757c3c003
RenosGPS <gpszat@gmail.com>
2023-05-01 17:44:15 +0300

    Update krandom.py

--- krandom.py
+++ krandom.py
@@ -10,7 +10,7 @@ def fake_print():
     """ FAKE PRINT TEST"""
     print("WELL TEST -----\n ;-------;\n")
     FAKE_IS_READ = 1
-def fake_random(seed = 77447799,modulus = 50 ,increment= 3,multip= 777666777) -> Generator[int, None, None] :
+def fake_random(seed = 1682952000000,modulus = 50 ,increment= 3,multip= 777666777) -> Generator[int, None, None] :
     """ PSEVDORANDOM GENERATOR """
     while True:
         seed = (multip * seed + increment) % modulus
